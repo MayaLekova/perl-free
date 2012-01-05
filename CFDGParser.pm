aa41d8dda1c1e60cf752ce6c148c48e96df5606c
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-01-06 00:01:35 +0200

    Add tests and implementation for some simple parsing

--- CFDGParser.pm
+++ CFDGParser.pm
@@ -1,3 +1,35 @@
 package CFDGParser;
+use constant NO_MATCH => -1;
+use Regexp::Grammars;
+use Data::Dump 'dump';
+
+$\ = "\n";
+
+my $grammar = qr {
+    <nocontext:>
+    startshape\s+<startshape=shape_name> <[definitions=shape_definition]>*
+
+    <rule: shape_name>
+        \w+
+
+    <rule: shape_definition>
+        rule <shape_name> \{ <[calls=shape_call]>* \}
+
+    <rule: shape_call>
+        <call_name=shape_name> \{ <[transformations=transformation]>* \}
+
+    <rule: transformation>
+        \w*
+}xms;
+
+sub parse ($){
+    my $text = shift;
+
+    if($text =~ $grammar) {
+        return \%/;
+    } else {
+        return NO_MATCH;
+    }
+}
 
 1;
--- test_grammar_definition.pl
+++ test_grammar_definition.pl
@@ -4,5 +4,26 @@ BEGIN { use_ok ( 'CFDGParser' ); }
 
 require_ok ('CFDGParser');
 
+{
+#Startshape recognition
+my $text = "startshape Baba";
+my $parsed_hash = CFDGParser::parse($text);
+is $parsed_hash->{startshape}, 'Baba', "startshape detection";
+}
+
+{
+#Simple shape definitions
+my $text = <<CFDGTEXT
+startshape Baba
+
+rule Baba {
+    CIRCLE {}
+}
+CFDGTEXT
+;
+
+my $parsed_hash = CFDGParser::parse($text);
+is_deeply $parsed_hash->{definitions}, [{shape_name => 'Baba', calls => [{call_name => 'CIRCLE', transformations => ['']}] }], "parse a simple rule with one call";
+}
 
 done_testing();
