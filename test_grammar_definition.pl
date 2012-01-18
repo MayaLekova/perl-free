fda32460d10253516a24e0bb124c542d6c21bb44
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-01-18 16:22:32 +0200

    Add more tests for transformations

--- test_grammar_definition.pl
+++ test_grammar_definition.pl
@@ -46,7 +46,7 @@ startshape Dyado
 
 rule Dyado {
     CIRCLE {}
-    TRIANGLE { x 5 y 10 size 7 2 }
+    TRIANGLE { x 5 y -10 size 7 2 }
 }
 CFDGTEXT
 ;
@@ -61,14 +61,49 @@ is_deeply $parsed_hash->{definitions}, [
                                                    },
                                                    {
                                                     call_name => 'TRIANGLE',
-                                                    transformations => [{ x => 5 },
-                                                                        { y => 10 },
-                                                                        { size => '7 2' }
+                                                    transformations => [{ cmd => 'x',    values => [5] },
+                                                                        { cmd => 'y',    values => [-10] },
+                                                                        { cmd => 'size', values => [7, 2] }
                                                                        ]
                                                    }
                                                   ]
                                         }
-                                       ]
+                                       ],
+                                       "parse rule with transformations";
+}
+
+{
+#Transformations with non-integers
+my $text = <<CFDGTEXT
+startshape Dyado
+
+rule Dyado {
+    CIRCLE {}
+    TRIANGLE { x 5 y 10 size 7.5 2 r 12.32 }
+}
+CFDGTEXT
+;
+
+my $parsed_hash = CFDGParser::parse($text);
+is_deeply $parsed_hash->{definitions}, [
+                                        {
+                                         shape_name => 'Dyado',
+                                         calls => [
+                                                   {
+                                                    call_name => 'CIRCLE',
+                                                   },
+                                                   {
+                                                    call_name => 'TRIANGLE',
+                                                    transformations => [{ cmd => 'x',    values => [5] },
+                                                                        { cmd => 'y',    values => [10] },
+                                                                        { cmd => 'size', values => [7.5, 2] },
+                                                                        { cmd => 'r',    values => [12.32] }
+                                                                       ]
+                                                   }
+                                                  ]
+                                        }
+                                       ],
+                                       "parse rule with transformations with noninteger arguments";
 }
 
 done_testing();
