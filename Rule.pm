fc3967247b8bca7dbcd7985310bf52a17fdafff4
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-01-28 00:53:40 +0200

    Fix mistake in Rule, passing all tests so far
    Context is a BITCH!

--- Rule.pm
+++ Rule.pm
@@ -1,11 +1,12 @@
 package Rule;
 use ShapeCall;
+use Data::Dump qw[dump];
 
 sub new {
     my $class = shift;
     my $self = shift;
 
-    $self->{calls} = [ map { ShapeCall->new($_) } $self->{calls} ];
+    $self->{calls} = [ map { ShapeCall->new($_) } @{$self->{calls}} ];
     bless $self, $class;
 }
 
