19ae2aaa77c90c39aca0cb8cf07722201bf91664
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-01-28 00:53:14 +0200

    Add test for Rule

--- test_classes.pl
+++ test_classes.pl
@@ -11,6 +11,7 @@ require_ok ('ShapeCall');
 require_ok ('Rule');
 require_ok ('CFDGParser');
 
+print "\n\n\n";
 
 subtest "Transformation" => sub {
     my $color_transform = Transformation->new({cmd => 'alpha', values => [0.5]});
@@ -20,17 +21,31 @@ subtest "Transformation" => sub {
     is $geometric_transform->type, "geometric", "detects geometric type transformations";
 };
 
+print "\n\n";
+
 subtest "ShapeCall" => sub {
     my $shape_call = ShapeCall->new( { call_name => 'CIRCLE', transformations => [ { cmd => 'x', values => [2] } ] } );
-    isa_ok($shape_call->{transformations}->[0], Transformation) || diag explain $shape_call;
+    isa_ok($shape_call->{transformations}->[0], Transformation);
 };
 
+print "\n\n";
 
 subtest "Rule" => sub {
-    my $text = "startshape Baba;\nrule Baba {\n \nCIRCLE { x 2 y 3 }\nSQUARE { s 0.7 }\n}\n";
-    my $syntax_tree = CFDGParser->parse($text);
+    my $text = <<TEXT
+    startshape Baba
+    rule Baba {
+        CIRCLE { x 2 y 3 }
+        SQUARE { s 7 }
+    }
+TEXT
+;
+    my $syntax_tree = CFDGParser::parse($text);
+
     my $baba = Rule->new($syntax_tree->{definitions}->[0]);
     isa_ok($baba, Rule) || diag explain $baba;
+    is ($baba->{shape_name}, 'Baba', 'the rule has the name given');
+    isa_ok($baba->{calls}->[0], ShapeCall);
+    is_deeply $baba->{calls}->[0]->{transformations}->[0], { cmd => 'x', values => [2] }, "transformations are kept in the right places";
 };
 
 done_testing();
