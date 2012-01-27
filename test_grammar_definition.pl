092810a11ea7686c42162eef03d6c112e2a17782
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-01-28 00:15:01 +0200

    Refactor parser tests
    Renamed the text variables and resulting hashes,
    so there's no reusing of variables

--- test_grammar_definition.pl
+++ test_grammar_definition.pl
@@ -7,8 +7,8 @@ require_ok ('CFDGParser');
 subtest "No transformations", sub {
     #Startshape recognition
     my $text1 = "startshape Baba";
-    my $parsed_hash = CFDGParser::parse($text1);
-    is $parsed_hash->{startshape}, 'Baba', "startshape detection";
+    my $parsed_hash1 = CFDGParser::parse($text1);
+    is $parsed_hash1->{startshape}, 'Baba', "startshape detection";
 
     #Simple shape definitions
     my $text2 = <<CFDGTEXT
@@ -21,8 +21,8 @@ subtest "No transformations", sub {
 CFDGTEXT
     ;
 
-    my $parsed_hash = CFDGParser::parse($text2);
-    is_deeply $parsed_hash->{definitions},  [
+    my $parsed_hash2 = CFDGParser::parse($text2);
+    is_deeply $parsed_hash2->{definitions},  [
                                              {
                                               shape_name => 'Baba',
                                               calls => [{
@@ -49,8 +49,8 @@ subtest "With transformations", sub {
 CFDGTEXT
     ;
 
-    my $parsed_hash = CFDGParser::parse($text3);
-    is_deeply $parsed_hash->{definitions}, [
+    my $parsed_hash3 = CFDGParser::parse($text3);
+    is_deeply $parsed_hash3->{definitions}, [
                                             {
                                              shape_name => 'Dyado',
                                              calls => [
@@ -81,8 +81,8 @@ CFDGTEXT
 CFDGTEXT
     ;
 
-    my $parsed_hash = CFDGParser::parse($text4);
-    is_deeply $parsed_hash->{definitions}, [
+    my $parsed_hash4 = CFDGParser::parse($text4);
+    is_deeply $parsed_hash4->{definitions}, [
                                             {
                                              shape_name => 'Dyado',
                                              calls => [
@@ -102,6 +102,78 @@ CFDGTEXT
                                             }
                                            ],
                                            "parse rule with transformations with noninteger arguments";
+
+    #Something a bit longer
+    my $text5 = <<CFDGTEXT
+    startshape Dyado
+
+    rule Dyado {
+        CIRCLE { x 2 y 0.1 }
+        SQUARE { size 1.1 }
+    }
+
+    rule STOP {}
+
+    rule Dyado {
+        STOP{}
+    }
+
+    rule Dyado {
+        SQUARE {}
+        Dyado { s 0.8 x 1.2 }
+    }
+
+CFDGTEXT
+;
+    my $parsed_hash5 = CFDGParser::parse($text5);
+
+    is_deeply $parsed_hash5->{definitions}, [
+                                             {
+                                              shape_name => 'Dyado',
+                                              calls => [
+                                                        {
+                                                         call_name => 'CIRCLE',
+                                                         transformations => [
+                                                                             { cmd => 'x', values => [2] },
+                                                                             { cmd => 'y', values => [0.1] },
+                                                                            ]
+                                                        },
+                                                        {
+                                                         call_name => 'SQUARE',
+                                                         transformations => [
+                                                                             { cmd => 'size', values => [1.1] },
+                                                                            ]
+                                                        }
+                                                       ]
+                                             },
+                                             {
+                                              shape_name => 'STOP',
+                                             },
+                                             {
+                                              shape_name => 'Dyado',
+                                              calls => [
+                                                        {
+                                                         call_name => 'STOP',
+                                                        },
+                                                       ],
+                                             },
+                                             {
+                                              shape_name => 'Dyado',
+                                              calls => [
+                                                        {
+                                                         call_name => 'SQUARE',
+                                                        },
+                                                        {
+                                                         call_name => 'Dyado',
+                                                         transformations => [
+                                                                             { cmd => 's', values => [0.8] },
+                                                                             { cmd => 'x' , values => [1.2] },
+                                                                            ],
+                                                        },
+                                                       ],
+                                             },
+                                            ],
+                                            "parse a richer grammar";
 };
 
 done_testing();
