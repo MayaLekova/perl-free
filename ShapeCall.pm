ac3968dae7f2091de4a22b7788792cb1a09fa448
Evgeni Kunev <evgeni.kunev@gmail.com>
2012-02-03 10:00:52 +0200

    Butify ShapeCall.pm a bit

--- ShapeCall.pm
+++ ShapeCall.pm
@@ -15,49 +15,43 @@ sub new {
 sub to_svg {
     my ($self, $parent) = @_;
     my $transform_str = "", my $h = 0, my $s = 0, my $v = 0;
-    foreach my $tr (@{$self->{transformations}})
-    {
-	if($tr->type() eq 'geometric')
-	{
-	    $transform_str = $transform_str.$tr->to_svg().", ";
-	}
-	elsif($tr->type() eq 'color')
-	{
+    foreach my $tr (@{$self->{transformations}}) {
+        if($tr->type() eq 'geometric') {
+            $transform_str = $transform_str.$tr->to_svg().", ";
+        }
+        elsif($tr->type() eq 'color') {
             ($h, $s, $v) = $tr->to_hsv($h, $s, $v);
-	}
+        }
     }
-    if(length($transform_str) > 0)
-    {
-	$transform_str = substr($transform_str, 0, -2);
+
+    if(length($transform_str) > 0) {
+        $transform_str = substr($transform_str, 0, -2);
     }
     my $color_str = Transformation::to_rgb($h, $s, $v);
 
     my $group = $parent->tag('g', 
-		style => {fill   => $color_str, stroke => $color_str },
-		transform => $transform_str);
+        style => {fill   => $color_str, stroke => $color_str },
+        transform => $transform_str);
     
-    if($self->{call_name} =~ 'TRIANGLE')
-    {
-	my $xv = [-0.5 * SCALE, 0.5 * SCALE, 0];
-	my $yv = [-0.5 * SCALE, -0.5 * SCALE, 0.5];
-		
-	my $points = $parent->get_path(
-            x=>$xv, y=>$yv,
-            -type=>'polygon',
-            -closed=>'true'
-        );
+    if($self->{call_name} =~ 'TRIANGLE') {
+        my $xv = [-0.5 * SCALE, 0.5 * SCALE, 0];
+        my $yv = [-0.5 * SCALE, -0.5 * SCALE, 0.5];
+            
+        my $points = $parent->get_path(
+                x=>$xv, y=>$yv,
+                -type=>'polygon',
+                -closed=>'true'
+            );
 
-	$group->polygon(%$points, 
-		style => {fill   => $color_str, stroke => $color_str },
-		transform => $transform_str);
+        $group->polygon(%$points, 
+            style => {fill   => $color_str, stroke => $color_str },
+            transform => $transform_str);
     }
-    elsif($self->{call_name} =~ 'SQUARE')
-    {
-	$group->rect(width => SCALE, height => SCALE);
+    elsif($self->{call_name} =~ 'SQUARE') {
+        $group->rect(width => SCALE, height => SCALE);
     }
-    elsif ($self->{call_name} =~ 'CIRCLE')
-    {
-	$group->circle(cx=>0, cy=>0, r => SCALE);
+    elsif ($self->{call_name} =~ 'CIRCLE') {
+        $group->circle(cx=>0, cy=>0, r => SCALE);
     }
     #TODO: find recursive shape call
 }
